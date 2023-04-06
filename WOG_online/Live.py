a8c88c01db2c6f38eb0adbff935552dec2d46f4d
Amit Menashe <amitmenashe@gmail.com>
2023-04-06 20:13:37 +0300

    fixed memory game and minor bugs, roulette left

--- WOG_online/Live.py
+++ WOG_online/Live.py
@@ -16,14 +16,20 @@ def MemoryGame(difficulty):
             numbers_list[i] = str(randint(1, 101))
         return numbers_list
 
+    def is_list_equal(difficulty,generated_number_list, user_number_list):
+        list_equal = True
+        generated_number_list.sort()
+        user_number_list.sort()
+        for number in range(int(difficulty)):
+            if not user_number_list[number] == generated_number_list[number] and list_equal:
+                list_equal = False
+        return list_equal
 
-    if "numbers_shown" not in session:
-        game = "MemoryGame"
-        generated_number_list = generate_sequence(difficulty)
+    if "generated_number_list" not in session:
+        session['generated_number_list'] = generate_sequence(difficulty)
         session['user_guess_length'] = int(difficulty)
         session['guess_list'] = [None] * session['user_guess_length']
-        session['numbers_shown'] = True
-        return render_template('memorygame_show.html', game=game, message=generated_number_list, difficulty=difficulty)
+        return render_template('memorygame_show.html', game=session['game_played'], message=session['generated_number_list'], difficulty=difficulty)
 
 
     while session['user_guess_length'] >0:
@@ -31,28 +37,28 @@ def MemoryGame(difficulty):
         user_guess = request.form.get('user_input', 'a')
 
         if user_guess.isdigit() and 1 <= int(user_guess) <= 101:
-            session['guess_list'][user_guess_length-1] = int(user_guess)
+            session['guess_list'][session['user_guess_length']-1] = user_guess
             session['user_guess_length']-=1
 
         game_message = f"Enter a number between 1 and 101, you need to enter {session['user_guess_length']} more numbers:\n"
         if session['user_guess_length']>0:
-            return render_template('game.html', game=game, message_request=game_message, difficulty=difficulty)
+            return render_template('game.html', game=session['game_played'], message_request=game_message, difficulty=difficulty)
         else:
             break
 
-    game_response=f"the numbers were: {generated_number_list}"
+    game_response=f"the numbers were: {session['generated_number_list']}"
+    session['outcome'] = is_list_equal(difficulty,session['generated_number_list'],session['guess_list'])
+    del session['generated_number_list']
     del session['user_guess_length']
     del session['guess_list']
-    del session['numbers_shown']
     #check if lists equal and get outcome
 
 
-    return render_template('game.html',game=game, message_response=game_response)
+    return render_template('game.html',game=session['game_played'], message_response=game_response)
 @app.route('/GuessGame/<int:difficulty>', methods=['GET', 'POST'])
 def GuessGame(difficulty):
 
     if 'secret_number' not in locals():
-        game="GuessGame"
         secret_number = randint(1, int(difficulty))
         game_message = f"please enter your guess, a number between 1 and {difficulty}:"
 
@@ -61,9 +67,9 @@ def GuessGame(difficulty):
     if user_guess.isdigit() and (1 <= int(user_guess) <= int(difficulty)):
         random_number_msg = (f"the number drawn was : {secret_number}")
         session['outcome'] = user_guess == str(secret_number)
-        return render_template('game.html',game=game, message_response=random_number_msg)
+        return render_template('game.html',game=session['game_played'], message_response=random_number_msg)
     else:
-        return render_template('game.html',game=game, message_request=game_message,difficulty=difficulty)
+        return render_template('game.html',game=session['game_played'], message_request=game_message,difficulty=difficulty)
 
 @app.route('/CurrencyRouletteGame/<int:difficulty>', methods=['GET', 'POST'])
 def CurrencyRouletteGame(difficulty):
@@ -101,11 +107,14 @@ def menu():
         session['game_played'] = game_played
 
     #if game chosen, check difficulty is valid
-    if session.get('game_played'):
+    if 'game_played' in session:
         if not game_difficulty is None and (game_difficulty.isdigit() and 1 <= int(game_difficulty) <= 5):
-            game_played=games_list[session['game_played']]
-            del session['game_played']
-            return render_template('GameSelect.html',game_played=game_played,difficulty=game_difficulty)
+            if session['game_played'].isdigit():
+                session['game_played']=games_list[session['game_played']]
+                return render_template('GameSelect.html',game_played=session['game_played'],difficulty=game_difficulty)
+            else:
+                return render_template('GameSelect.html', game_played=session['game_played'],difficulty=game_difficulty)
+
 
         else:
             difficulty_message = "Please choose game difficulty from 1 (easy) to 5 (hard)"
@@ -119,12 +128,15 @@ def menu():
 
 @app.route('/outcome',methods=['GET', 'POST'])
 def outcome():
-            outcome = session['outcome']
-            if not outcome:
-                return render_template('endgame.html', message="you lost!")
-            else:
-                del session['outcome']
-                return render_template('endgame.html', message="you won!")
+
+    del session['game_played']
+    outcome= session['outcome']
+    del session['outcome']
+    if not outcome:
+        return render_template('endgame.html', message="you lost!")
+    else:
+
+        return render_template('endgame.html', message="you won!")
 
 
 
