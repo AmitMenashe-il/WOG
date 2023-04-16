6867ab2f4424d3c221d87c53ec54f0e231d5c693
Amit Menashe <amitmenashe@gmail.com>
2023-04-16 14:20:02 +0300

    working on online

---
+++ WOG_local (old)/MainGame.py
@@ -0,0 +1,7 @@
+from Live import load_game, welcome
+
+# prints a welcome messege
+welcome(input("Please enter your name\n"))
+
+# prints games menu, returns game and difficulty as a chained string
+load_game()
